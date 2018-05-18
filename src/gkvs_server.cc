41a9126e3429d7fc773cf7e7af7da694035b6a04
Alex Shvid <a@shvid.com>
2018-05-17 21:30:09 -0700

    Create gkvs_server.cc

---
+++ src/gkvs_server.cc
@@ -0,0 +1,49 @@
+/*
+ *
+ * Copyright 2015 gKVS authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpc/grpc.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+#include <grpcpp/security/server_credentials.h>
+#include "helper.h"
+#include "gkvs.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerReader;
+using grpc::ServerReaderWriter;
+using grpc::ServerWriter;
+using grpc::Status;
+using std::chrono::system_clock;
+
+
+int main(int argc, char** argv) {
+
+  std::cout << "GKVS Server" << std::endl;
+
+  return 0;
+}
