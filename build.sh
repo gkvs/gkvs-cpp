426b7b1a8b3516e5cb75d0b7a7733054fb747685
Alex Shvid <a@shvid.com>
2018-07-03 16:35:35 -0700

    improve build process

--- build.sh
+++ build.sh
@@ -6,8 +6,6 @@ rm -rf build
 mkdir build
 cd build
 cmake ..
-
-pushd build
 make
-popd
+
 
--- macos-brew.sh
+++ macos-brew.sh
@@ -8,4 +8,12 @@ brew install autoconf automake libtool shtool
 
 brew install gflags
 
+brew install protobuf grpc glog
+
+brew install hiredis
+
+brew install rocksdb
+
+brew install luajit
+
 brew install cmake
